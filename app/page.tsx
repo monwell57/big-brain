a37678061791a63501505c688d3b70f345e55475
Gregory Peters <greg.monwell57@gmail.com>
2024-06-02 18:06:55 -0700

    Scooping Data To User Identifier

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 'use client';
 
+import { api } from '@/convex/_generated/api';
 import { SignInButton, UserButton } from '@clerk/nextjs';
-import { Authenticated, Unauthenticated } from 'convex/react';
+import {
+	Authenticated,
+	Unauthenticated,
+	useMutation,
+	useQuery,
+} from 'convex/react';
 import Image from 'next/image';
 
 export default function Home() {
+	const documents = useQuery(api.documents.getDocuments);
+	const createDocument = useMutation(api.documents.createDocument);
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<Unauthenticated>
@@ -12,6 +20,14 @@ export default function Home() {
 			</Unauthenticated>
 			<Authenticated>
 				<UserButton />
+				<button
+					onClick={() => {
+						createDocument({ title: 'Hello World' });
+					}}
+				>
+					Click Me
+				</button>
+				{documents?.map((doc) => <div key={doc._id}>{doc.title}</div>)}
 			</Authenticated>
 		</main>
 	);
---
+++ convex/documents.ts
@@ -0,0 +1,30 @@
+import { mutation, query } from './_generated/server';
+import { ConvexError, v } from 'convex/values';
+
+export const getDocuments = query({
+	async handler(ctx) {
+		const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier;
+		if (!userId) {
+			return [];
+		}
+		return await ctx.db
+			.query('documents')
+			.withIndex('by_tokenIdentifier', (q) => q.eq('tokenIdentifier', userId))
+			.collect();
+	},
+});
+
+// Create a new task with the given text
+export const createDocument = mutation({
+	args: { title: v.string() },
+	async handler(ctx, args) {
+		const userId = (await ctx.auth.getUserIdentity())?.tokenIdentifier;
+		if (!userId) {
+			throw new ConvexError('Not authenticated');
+		}
+		await ctx.db.insert('documents', {
+			title: args.title,
+			tokenIdentifier: userId,
+		});
+	},
+});
